11dbf02ad6960a5cdffc07cbe63e9b35fc54ce96
Rob Prouse <rob@prouse.org>
2014-10-29 16:38:37 -0400

    A simple event view

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+eventsApp.controller('EventController',
+  function EventController($scope) {
+    $scope.event = {
+      name: 'CoderCamp 21',
+      date: '18/11/2014',
+      time: '6:30 pm',
+      location: {
+        address: 'Augusta St',
+        city: 'Hamilton',
+        province: 'ON'
+      },
+      imageUrl: 'http://www.softwarehamilton.com/wp-content/uploads/2014/01/swhamsmall.jpg'
+    }
+});
