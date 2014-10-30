0680fa176884483a357ae770a0cfbea0d49392f6
Rob Prouse <rob@prouse.org>
2014-10-30 16:06:48 -0400

    Added filters to the event sessions

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,9 +1,12 @@
 ﻿(function () {
 
-  eventsApp.controller('EventController', function($scope) {
+  eventsApp.controller('EventController', function ($scope) {
+
+    $scope.sortorder = 'name';
+
     $scope.event = {
       name: 'CoderCamp 21',
-      date: '18/11/2014',
+      date: 1421989200000,
       time: '6:30 pm',
       location: {
         address: 'Augusta St',
@@ -15,23 +18,30 @@
         {
           name: 'Introduction to HTML',
           creatorName: 'Bob Smith',
-          duration: '30 mins',
+          duration: 1,
           level: 'Introductory',
           abstract: 'In this sesion, you will learn to love angle brackets',
           upVoteCount: 0
         }, {
           name: 'Node.js for .NET Developers',
           creatorName: 'Joanne Jones',
-          duration: '1 hour',
+          duration: 2,
           level: 'Intermediate',
           abstract: 'Any web developer worth their salt is using Node.js these days',
           upVoteCount: 0
         }, {
           name: 'JQuery Fundementals',
           creatorName: 'Gary White',
-          duration: '2 hours',
+          duration: 1,
           level: 'Intermediate',
           abstract: 'A quick dive into using JQuery',
+          upVoteCount: 0
+        }, {
+          name: 'JQuery Deep Dive',
+          creatorName: 'Gary White',
+          duration: 4,
+          level: 'Advanced',
+          abstract: 'Continuing from his first session, Gary takes a deep dive into JQuery',
           upVoteCount: 0
         }
       ]
--- app/js/filters.js
+++ app/js/filters.js
@@ -1,2 +1,18 @@
 'use strict';
 
+eventsApp.filter('durations', function () {
+  return function (duration) {
+    switch (duration) {
+      case 1:
+        return "1 Hour";
+      case 2:
+        return "2 Hours";
+      case 3:
+        return "Half-Day";
+      case 4:
+        return "Full-Day";
+      default:
+        return "TBA";
+    }
+  }
+});
