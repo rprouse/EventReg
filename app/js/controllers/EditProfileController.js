7d195dfa16cdb679958249b2565b358d0ff94efc
Rob Prouse <rob@prouse.org>
2014-10-31 14:16:51 -0400

    A simple edit profile page

---
+++ app/js/controllers/EditProfileController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+  'use strict';
+
+  eventsApp.controller('EditProfileController', EditProfileController);
+
+  EditProfileController.$inject = ['$scope', 'gravatarUrlBuilder'];
+
+  function EditProfileController($scope, gravatarUrlBuilder) {
+    $scope.user = {};
+
+    $scope.getGravatarUrl = function (email) {
+      return gravatarUrlBuilder.buildGravatarUrl(email);
+    }
+    activate();
+
+    function activate() { }
+  }
+})();
---
+++ app/js/services/GravatarUrlBuilder.js
@@ -0,0 +1,23 @@
+﻿(function () {
+  'use strict';
+
+  eventsApp.factory('gravatarUrlBuilder', GravatarUrlBuilder);
+
+  GravatarUrlBuilder.$inject = ['$http'];
+
+  function GravatarUrlBuilder($http) {
+    var service = {
+        buildGravatarUrl: function(email) {
+          var defaultGravatarUrl = "http://www.gravatar.com/avatar/000?s=200";
+
+          var regex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/;
+          if (!regex.test(email))
+            return defaultGravatarUrl;
+
+          return 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email) + '.jpg?s=200&r=g';
+        }
+    };
+
+    return service;
+  }
+})();
