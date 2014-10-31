44c7d891e509cb90e557d986d28e03a9b2153e03
Rob Prouse <rob@prouse.org>
2014-10-31 09:54:29 -0400

    Created a create event form

---
+++ app/js/controllers/EditEventController.js
@@ -0,0 +1,22 @@
+﻿(function () {
+  'use strict';
+
+  eventsApp.controller('EditEventController', EditEventController);
+
+  EditEventController.$inject = ['$scope'];
+
+  function EditEventController($scope) {
+
+    $scope.saveEvent = function(event) {
+      alert(event.name);
+    }
+
+    $scope.cancelEdit = function() {
+      window.location = "/EventDetails.html";
+    }
+
+    activate();
+
+    function activate() { }
+  }
+})();
