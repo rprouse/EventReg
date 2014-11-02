10ffd60a75cf55936d9c5ba3880eac41ad6790d5
Rob Prouse <rob@prouse.org>
2014-11-02 18:13:49 -0500

    Added an <event-thumbnail> directive

---
+++ app/js/directives/eventThumbnail.js
@@ -0,0 +1,20 @@
+﻿(function () {
+  'use strict';
+
+  eventsApp.directive('eventThumbnail', eventThumbnail);
+
+  eventThumbnail.$inject = [];
+
+  function eventThumbnail() {
+    // Usage:
+    //     <event-thumbnail />
+    // Creates:
+    // 
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/directives/EventThumbnail.html'
+    };
+  }
+
+})();
