0c4b7847c8fda73ecdc53a463f500b618015d3fa
Rob Prouse <rob@prouse.org>
2014-11-01 22:23:21 -0400

    Added the first route

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngResource', 'ui.bootstrap']);
+var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute', 'ui.bootstrap'])
+  .config(function($routeProvider) {
+    $routeProvider.when('/newEvent',
+    {
+      templateUrl: 'templates/NewEvent.html',
+      controller: 'EditEventController'
+    });
+});
+
+
